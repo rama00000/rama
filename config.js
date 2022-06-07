51c198b4a3bfa612fc7b1a065d245168a9718a6f
rama00000 <105921359+rama00000@users.noreply.github.com>
2022-06-07 11:37:02 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,74 @@
+// Thanks For Allah
+// AlyaaXzy
+// BOTCAHX
+// Zifabotz
+
+let fs = require('fs')
+global.owner = ['6287793236775', '6287793236775','6287793236775', '6287793236775', '6287793236775'] // Letakan nomor kamu disini
+global.APIs = { // API Prefix
+  // nama: 'https://website'
+  hardianto: 'https://hardianto-chan.herokuapp.com',
+  rey: 'https://server-api-rey.herokuapp.com',
+  jonaz: 'https://jonaz-api-v2.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  nrtm: 'https://nurutomo.herokuapp.com',
+  pencarikode: 'https://pencarikode.xyz',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zekais: 'http://zekais-api.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  vhtear: 'https://api.vhtear.com',
+  lolhum: 'https://api.lolhuman.xyz',
+  fxc7: 'https://fxc7-api.herokuapp.com',
+  bx: 'https://bx-hunter.herokuapp.com',
+
+}
+global.APIKeys = { // APIKey nya disini
+  // 'https://website': 'apikey'
+  'https://server-api-rey.herokuapp.com': 'apirey',
+  'https://hardianto-chan.herokuapp.com': 'hardianto',
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://pencarikode.xyz': 'APIKEY',
+  'https://api.xteam.xyz': 'HIRO',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'nyyxz-bot',
+  'https://api.lolhuman.xyz': '2d118bcd18c4e779748a84b',
+  'https://api.vhtear.com': 'sayahafiz',
+  'https://fxc7-api.herokuapp.com': 'uN8rsK4g',
+  'https://api.justaqul.xyz': '5kbUqdG00OXelFYx',
+  'http://zekais-api.herokuapp.com': 'grqgD6pU',
+  'https://bx-hunter.herokuapp.com': 'Ikyy69',
+}
+
+// Sticker WM
+global.packname = 'RamaBotz'
+global.author = 'Rama'
+
+global.wait = '▰▰▰▰▱▱▱▱▱▱ ʟᴏᴀᴅɪɴɢ...'
+global.eror = '「❗」Server LAGI ERROR COK!'
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
+global.watermark = '©️ RamaBotz' //change the watermark 
+global.image = 'https://telegra.ph/file/052ca138a613c8859756f.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
+
+// image
+global.bcloc = 'https://telegra.ph/file/c3bcc6f8e8574d1c5274f.jpg'
+global.donasi = 'https://telegra.ph/file/64d8556e1645572a455a5.jpg'
+global.ow = 'https://telegra.ph/file/ae93a09d51c672afa8eb4.jpg'
+global.menu = 'https://telegra.ph/file/d487aca6b46c1282c250a.jpg'
+global.rules = 'https://telegra.ph/file/fc0bcf7e40cb0aa9ff64f.jpg'
+global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
+global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
+global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
+global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
+
+global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
